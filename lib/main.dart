64a7d6458eedb56930729a76567d1c0aed67e517
Abdulqudr <www.olarboy45@gmail.com>
2025-11-11 02:09:22 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: Colors.grey,
         ),
-        title: 'Flutter Demo',
+        title: 'App Demo',
         home: DashboardScreen());
   }
 }
